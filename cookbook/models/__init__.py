09fc9d76968f5c85f2e1a923167f0e4b91bc4b0f
swarog8777 <swarog8777@gmail.com>
2026-06-16 20:21:08 +0700

    Delete cookbook/models directory

--- cookbook/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from cookbook.models.base import Base
-from cookbook.models.ingredient import Ingredient
-from cookbook.models.recipe import Recipe
-from cookbook.models.recipe_ingredients import RecipeIngredient
-
-__all__ = ["Base", "Recipe", "Ingredient", "RecipeIngredient"]
