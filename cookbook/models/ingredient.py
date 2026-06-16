7f5e2f573b80c52be85d667a9c1db6d8d0173a88
Sergei <usn@example.com>
2026-06-16 13:28:06 +0700

    created tables

--- cookbook/models/ingredient.py
+++ cookbook/models/ingredient.py
@@ -1,7 +1,7 @@
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from models.base import Base
+from cookbook.models.base import Base
 
 
 class Ingredient(Base):
