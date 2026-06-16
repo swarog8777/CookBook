90935ff5867e1a28c8cb9558540e96cbdf9734ac
Sergei <usn@example.com>
2026-06-16 22:50:41 +0700

    init3

--- cookbook/repositories/ingredient_repository.py
+++ cookbook/repositories/ingredient_repository.py
@@ -11,25 +11,24 @@ class IngredientRepository:
         result = await db.execute(stmt)
         return result.scalar_one_or_none()
 
-
     @staticmethod
     async def get_by_id(db: AsyncSession, ingredient_id: int):
         stmt = select(Ingredient).where(Ingredient.id == ingredient_id)
         result = await db.execute(stmt)
         return result.scalar_one_or_none()
-    
+
     @staticmethod
     async def get_all(db: AsyncSession):
         stmt = select(Ingredient).order_by(Ingredient.id)
         result = await db.execute(stmt)
         return result.scalars().all()
-    
+
     @staticmethod
     async def create(db: AsyncSession, ingredient: Ingredient):
         db.add(ingredient)
         await db.flush()
         return ingredient
-    
+
     @staticmethod
     async def delete(db: AsyncSession, ingredient: Ingredient):
         db.delete(ingredient)
--- cookbook/repositories/recipe_repository.py
+++ cookbook/repositories/recipe_repository.py
@@ -1,19 +1,26 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
+
 from cookbook.models import Recipe
 
 
 class RecipeRepository:
     @staticmethod
     async def get_all(db: AsyncSession):
-        stmt = select(Recipe).options(selectinload(Recipe.ingredients)).order_by(Recipe.id)
+        stmt = (
+            select(Recipe).options(selectinload(Recipe.ingredients)).order_by(Recipe.id)
+        )
         result = await db.execute(stmt)
         return result.scalars().unique().all()
 
     @staticmethod
     async def get_by_id(db: AsyncSession, recipe_id: int):
-        stmt = select(Recipe).where(Recipe.id == recipe_id).options(selectinload(Recipe.ingredients))
+        stmt = (
+            select(Recipe)
+            .where(Recipe.id == recipe_id)
+            .options(selectinload(Recipe.ingredients))
+        )
         result = await db.execute(stmt)
         return result.scalar_one_or_none()
 
@@ -22,17 +29,12 @@ class RecipeRepository:
         db.add(recipe)
         await db.flush()
         return recipe
-    
+
     @staticmethod
     async def delete(db: AsyncSession, recipe: Recipe):
         db.delete(recipe)
-    
-    
+
     @staticmethod
     async def update(db: AsyncSession, recipe: Recipe):
         await db.flush()
         return recipe
-
-
-    
-    
