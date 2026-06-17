f7c59a89ab2e703cc947ff5287f6d81fcabb2841
Sergei <usn@example.com>
2026-06-17 08:06:57 +0700

    init 3

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# CookBook
+# Настройка проекта CookBook
+
+Клонирование репозитория
+```bash
+git clone git@github.com:swarog8777/CookBook.git
+```
+Открыть проект и в корне создать .env
+```
+POSTGRES_USER=user
+POSTGRES_PASSWORD=1234
+POSTGRES_DB=cookbookdb
+POSTGRES_HOST=localhost
+POSTGRES_PORT=5432
+```
+```bash
+uv install
+```
---
+++ tests/conftest.py
@@ -0,0 +1,33 @@
+import asyncio
+
+import pytest
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+from cookbook.models import Base
+
+
+@pytest.fixture(scope="session")
+def event_loop():
+    loop = asyncio.new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest.fixture(scope="function")
+async def db():
+    engine = create_async_engine(
+        "sqlite+aiosqlite:///:memory:", echo=False, future=True
+    )
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    
+    AsyncSessionLocal = sessionmaker(
+        bind=engine,
+        class_=AsyncSession,
+        expire_on_commit=False,
+    )
+
+    async with AsyncSessionLocal() as session:
+        yield session
+        await session.close()
